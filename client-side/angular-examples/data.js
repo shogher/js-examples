204267cafc3f877d854c0e2f19e5259b9391e67d
shogher <shogher.baghryan@gmail.com>
2015-10-31 22:26:21 +0400

    Update data.js

--- client-side/angular-examples/data.js
+++ client-side/angular-examples/data.js
@@ -1,4 +1,5 @@
-angular.module("myApp", [])
-.controller("TreeController", ['$scope', function($scope) {
+angular.module("myApp", []).controller("TreeController", ['$scope', function($scope) {
 	$scope.tree =treedata;
+	$scope.head = [{title:'Գնման ենթակա  ապրանքներ, աշխատանքներ և ծառայություններ',sClass:""}, {title: 'Գնման առարկան',sClass:""}, {title: 'Չափման միավորը',sClass: 'text-center'}, {title: 'Ամբողջ քանակը (ծավալը)',sClass: 'text-right'}, {title: 'Ընդհանուր գումարը (հազ. դրամով)',sClass: 'text-right'}, {title: 'Գնման ձևը (ընթացակարգը)',sClass: 'text-right'}];
+	$scope.table =dataSet;
 }]);
